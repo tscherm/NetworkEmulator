95a9cbf1f7e612bd343b749de4b448fa4af52583
Tom Schermerhorn <tomjschermerhorn@gmail.com>
2024-11-05 21:21:50 -0600

    check port ranges in emulator

--- emulator.py
+++ emulator.py
@@ -1,4 +1,7 @@
 import argparse
+import sys
+import socket
+from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser(description="Network Emulator")
 
@@ -8,3 +11,11 @@ parser.add_argument("-q", "--queue_size", type=int, required=True, dest="queueSi
 parser.add_argument("-l", "--log", type=str, required=True, dest="logName")
 
 args = parser.parse_args()
+
+# check port numbers
+if 2049 > args.port or args.port > 65536:
+    print("Sender port out of range.")
+    sys.exit()
+if 2049 > args.port or args.port > 65536:
+    print("Requester port out of range.")
+    sys.exit()
